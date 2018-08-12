d6b4f856a62ebe29753acd46bd521f58b57e4822
GeorgeK95 <george_it@abv.bg>
2018-08-12 12:43:53 +0300

    Initial commit, created both server and client projects.

---
+++ real-estate-bidding-system-api/src/main/java/org/universe/realestatebiddingsystem/RealEstateBiddingSystemApplication.java
@@ -0,0 +1,12 @@
+package org.universe.realestatebiddingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBiddingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateBiddingSystemApplication.class, args);
+    }
+}
